80c639b80fbb6a2cb02cbbed5e6809a26f1e31bb
George Lesica <george@lesica.com>
2018-11-18 07:50:03 -0700

    Start on recursive fetch

--- lib/src/pub/fetch.dart
+++ lib/src/pub/fetch.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:convert' show json;
 
 import 'package:http/http.dart' as http;
@@ -49,4 +50,72 @@ Future<Iterable<PackageVersion>> fetchPackageVersions(
   );
 
   return packageVersions;
+}
+
+Future<Iterable<PackageVersion>> recursiveFetchPackageVersions(
+  String packageName, {
+  Cache cache,
+  bool https = true,
+  String source = defaultSource,
+}) async {
+  assert(packageName != null);
+  assert(https != null);
+  assert(source != null);
+
+  final packageVersions = <PackageVersion>[];
+
+  final packageQueue = Queue.of([_QueuedPackage(packageName, source)]);
+  final packagesSeen = Set<_QueuedPackage>();
+
+  while (packageQueue.isNotEmpty) {
+    final nextQueuePackage = packageQueue.removeFirst();
+
+    // Check if we've seen this one before, if so, we're good to go,
+    // if not, record it and move on.
+    if (packagesSeen.contains(nextQueuePackage)) {
+      continue;
+    }
+    packagesSeen.add(nextQueuePackage);
+
+    final nextPackageVersions = await fetchPackageVersions(
+      nextQueuePackage.packageName,
+      cache: cache,
+      https: https,
+      source: nextQueuePackage.source,
+    );
+
+    for (final nextPackageVersion in nextPackageVersions) {
+      packageVersions.add(nextPackageVersion);
+
+      // Should we just hydrate the PackageVersion object with enough
+      // info about its dependencies to create relationships and not
+      // worry about hitting all the package versions up front? Either
+      // way this approach won't give us enough information to actually
+      // compute which dependency versions we should have relationships
+      // to because we're just going to end up with a flat list.
+
+      // Queue up dependencies
+      nextPackageVersion.dependencies.forEach((dependency) {
+        final nextQueueDependency = _QueuedPackage(dependency.name, );
+      });
+    }
+  }
+
+  return packageVersions;
+}
+
+class _QueuedPackage {
+  final String packageName;
+  final String source;
+
+  _QueuedPackage(this.packageName, this.source);
+
+  @override
+  int get hashCode => packageName.hashCode ^ source.hashCode;
+
+  @override
+  bool operator ==(dynamic other) =>
+      other is _QueuedPackage &&
+      other.packageName == packageName &&
+      other.source == source;
 }
