992486f810daa1e3f12723abcd1ff127d960c9bb
George Lesica <george@lesica.com>
2018-11-25 00:36:36 -0700

    Address various pana findings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.1
+
+Address various pana findings.
+
 ## 1.0.0
 
 Initial release.
--- test/database/retry_harness.dart
+++ test/database/retry_harness.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:pkgraph/src/database/retry.dart';
 import 'package:test/test.dart';
 
