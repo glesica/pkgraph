3c0cd369f337177e5a470e2044302ab6998bf4ac
George Lesica <george@lesica.com>
2018-11-24 23:49:07 -0700

    Add CI and simplify test script

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-pub run dart_style:format -l 80 -w --set-exit-if-changed lib/ test/
+dartfmt -l 80 -w --set-exit-if-changed lib/ test/
 dartanalyzer --fatal-infos --fatal-warnings lib/ test/
 pub run test -r expanded
